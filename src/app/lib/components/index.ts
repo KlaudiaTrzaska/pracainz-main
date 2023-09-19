0822eb93f36c36d55775fa21f04fcddba18679b8
Klaudia Bialczyk <klaudiab@backbase.com>
2023-09-19 12:30:23 +0200

    Remove redundant files

--- src/app/lib/components/index.ts
+++ src/app/lib/components/index.ts
@@ -1,3 +1,1 @@
-export * from './footer/footer.component';
 export * from './layouts/layout-horizontal/layout-horizontal.component';
-export * from './navbar/navbar.component';
--- src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.ts
+++ src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FooterComponent, NavbarComponent } from '@lib/components';
 
 @Component({
   selector: 'app-layout-horizontal',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule],
   templateUrl: './layout-horizontal.component.html',
   styleUrls: ['./layout-horizontal.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
